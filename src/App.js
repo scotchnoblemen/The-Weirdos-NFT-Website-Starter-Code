3d6a699c3d87fbf3c00b3ddf2976a27641302326
kwoods7 <67384199+kwoods714@users.noreply.github.com>
2022-03-12 14:33:46 -0600

    new site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import GlobalStyles from "./styles/GlobalStyles";
+import { light } from "./styles/Themes";
+import { ThemeProvider } from "styled-components";
+
+// import { lazy, Suspense } from "react";
+// import Loading from "./components/Loading";
+
+// Below Imports using React.lazy and Suspence
+
+// const Navigation = lazy(() => import("./components/Navigation"));
+// const Home = lazy(() => import("./components/sections/Home"));
+// const About = lazy(() => import("./components/sections/About"));
+// const Roadmap = lazy(() => import("./components/sections/Roadmap"));
+// const Team = lazy(() => import("./components/sections/Team"));
+// const Showcase = lazy(() => import("./components/sections/Showcase"));
+// const Faq = lazy(() => import("./components/sections/Faq"));
+// const Footer = lazy(() => import("./components/Footer"));
+// const ScrollToTop = lazy(() => import("./components/ScrollToTop"));
+
+import Navigation from "./components/Navigation";
+import About from "./components/sections/About";
+import Home from "./components/sections/Home";
+import Roadmap from "./components/sections/Roadmap";
+import Team from "./components/sections/Team";
+import Footer from "./components/Footer";
+import Showcase from "./components/sections/Showcase";
+import Faq from "./components/sections/Faq";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+        {/* <Suspense fallback={<Loading />}> */}
+          <Navigation />
+          <Home />
+          <About />
+          <Roadmap />
+          <Showcase />
+          <Team />
+          <Faq />
+          <Footer />
+          {/* <ScrollToTop scrollPosition={y}/> */}
+          <ScrollToTop />{" "}
+        {/* </Suspense> */}
+      </ThemeProvider>
+    </main>
   );
 }
 
