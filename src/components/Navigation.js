f113b9565492b8a76f5ea5383c1a088653c76b1e
kwoods7 <67384199+kwoods714@users.noreply.github.com>
2022-03-17 10:03:50 -0500

    don't remember

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -179,12 +179,12 @@ const scrollTo = (id) => {
           <MenuItem onClick={() => scrollTo('faq')}  >Faq</MenuItem>
           <MenuItem>
             <div className="mobile">
-            <button class="sc-bdvvtL hhmjnQ"><a href="http://www.scotchnoblemint.xyz" aria-label="Mint NFT" target="_blank" rel="noreferrer">Mint NFT</a></button>
+            <button class="sc-bdvvtL hhmjnQ"><a href="http://www.scotchnoblemint.xyz" aria-label="Mint NFT" target="_blank" rel="noreferrer">Mint Testnet NFT</a></button>
             </div>
           </MenuItem>
         </Menu>
           <div className="desktop">
-          <button class="sc-bdvvtL hhmjnQ"><a href="http://www.scotchnoblemint.xyz" aria-label="Mint NFT" target="_blank" rel="noreferrer">Mint NFT</a></button>
+          <button class="sc-bdvvtL hhmjnQ"><a href="http://www.scotchnoblemint.xyz" aria-label="Mint NFT" target="_blank" rel="noreferrer">Mint Testnet NFT</a></button>
           </div>
 
       </NavBar>
